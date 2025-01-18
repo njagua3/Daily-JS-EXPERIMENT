212950d694f6e998f18f29afc4d3806fa9974e12
njagua <njagua.gma@gmail.com>
2025-01-18 20:28:15 +0300

    thoughts

---
+++ 10_Event_Bubbling_&_Delegation/ebd.js
@@ -0,0 +1,19 @@
+/* BEFORE WRITING YOUR NEXT CODE VISUALIZE EVERYTHING IN YOUR MIND LIKE A STORY
+Its ALL  CONNECTED MF
+JUST THINK OF WHAT IT IS YOU WANT TO DO
+EG
+YOU WANT TO ADD A NEW LI TAG IN AN UNORDERED LIST 
+SO YOU MUST GET A REFERENCE TO THAT UL
+
+const ul = document.querySelector("ul");
+
+YOU ALSO NEED A REFERENCE TO THE BUTTON CLICKED FOR ADDING A NEW LI TAG 
+
+
+const button = document.querySelector("button");
+
+button.addEventListener("click", (e) => {
+e.target.classList.add("
+}
+
+*/
