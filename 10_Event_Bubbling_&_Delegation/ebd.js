00ce1fad6c829179912b2d222d0308b9ce54f977
njagua <njagua.gma@gmail.com>
2025-01-19 22:33:13 +0300

    self reflection pt 2

--- 10_Event_Bubbling_&_Delegation/ebd.js
+++ 10_Event_Bubbling_&_Delegation/ebd.js
@@ -15,7 +15,15 @@ const button = document.querySelector("button");
 button.addEventListener("click", (e) => {
 e.target.classList.add("
 }
+what is a stack and a heap? whats the difference
+global scope vs local scope
 
+window object vs the local object
+
+do you understand what we mean by normal fn cannot be defined in an object
+but methods can be defined in an object
+ do you understand an array of objects - basically this is what you will work with 90% 
+ 
 Try and visualize even the basics like math function what it does, its use cases, forEACH method application, the this keyword etc
 Programming is more of understanding what youre doing and less of coding
 Make sure you do that tomorrow and also make it a habit
