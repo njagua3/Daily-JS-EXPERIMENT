d834902f9f649d24ffe315256d03cd6b4214cad9
njagua <njagua.gma@gmail.com>
2025-01-27 13:39:27 +0300

    chapter 13 added

---
+++ 13__Events__Inside__Forms/eif.js
@@ -0,0 +1,26 @@
+/* 
+We currently have the knowledge of events and events listeners
+
+They are used a lot with web forms
+
+Web forms exist to capture some kind of data or info from a user
+
+eg sign in form, customer feedback form
+
+We are capturing data or info from a user
+
+We are going to use events to capture that information
+
+We are interested with the submit events of forms
+
+This is when a user clicks the submit button at the bottom 
+of the form once filled in
+
+Click in the submit button is not only a click event but also a
+submit event on the form itself
+
+Other kind of events that go hand in hand with web forms are;
+
+- Keyboard events - we can detect if a user presses or realeses a key on their keyboard
+       - we can also check if they have their alt or shift key pressed
+*/
