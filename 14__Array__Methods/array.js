e427b76ce0a6e60b3073eaa035b692b297c527f7
njagua <njagua.gma@gmail.com>
2025-02-18 13:43:43 +0300

    map method

--- 14__Array__Methods/array.js
+++ 14__Array__Methods/array.js
@@ -33,8 +33,34 @@ const premiumUsers = users.filter((user) => {
 });
 console.log(premiumUsers);
 
-
 /*
 Map Method - non destructive
             useful for creating new arrays
-*/
+   it takes an array and maps it into a completely new array         
+cycles through an array and creates a new array based on that array
+   */
+
+const prices = [20, 10, 30, 25, 15, 40, 80, 5];
+
+const salePrices = prices.map((price) => {
+  return price / 2;
+});
+console.log(salePrices);
+
+//Example 2
+const products = [
+  { name: "goldstar", price: 20 },
+  { name: "mushroom", price: 40 },
+  { name: "green shells", price: 30 },
+  { name: "banana skin", price: 10 },
+  { name: "red shells", price: 50 },
+];
+
+const newProducts = products.map((product) => {
+  if (product.price > 30) {
+    return { name: product.name, price: product.price / 2 };
+  } else {
+    return product;
+  }
+});
+console.log(newProducts);
